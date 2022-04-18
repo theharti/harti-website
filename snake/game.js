61b6f3fb838bc56f57541829d7211fa59b6b6c73
theharti <theharti2306@gmail.com>
2022-04-18 11:52:41 +0200

    snake game added

---
+++ snake/game.js
@@ -0,0 +1,41 @@
+import { update as updateSnake, draw as drawSnake, getSnakeHead,snakeIntersection } from './snake.js';
+import{SNAKE_SPEED} from './snake.js';
+import {update as updateFood, draw as drawFood} from './food.js';
+import {outsideGrid} from './grid.js'
+
+let lastRenderTime=0;
+const gameBoard=document.getElementById('game-board');
+let gameOver=false;
+
+function main(currentTime){
+    if(gameOver){
+        if(confirm('You Lost. Press ok to restart')){
+            window.location='';
+        }
+        return;
+    }
+
+    window.requestAnimationFrame(main);
+    const secondsSinceLastRender=(currentTime-lastRenderTime)/1000;
+    if(secondsSinceLastRender<1/SNAKE_SPEED)return;
+    
+    lastRenderTime=currentTime;
+
+    update();
+    draw();
+    
+}
+window.requestAnimationFrame(main);
+function update(){
+    updateSnake();
+    updateFood();
+    checkDeath();
+}
+function draw(){
+    gameBoard.innerHTML='';
+    drawSnake(gameBoard);
+    drawFood(gameBoard)
+}
+function checkDeath(){
+    gameOver=outsideGrid(getSnakeHead()) ||snakeIntersection();
+}
